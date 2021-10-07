0d4388688df307fc75c4276e9a2df5e0ce9787c2
Craig <reeseca90@gmail.com>
2021-10-07 17:01:53 -0400

    basic UI created, no eventListeners done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-import shipFactory from './shipFactory'
-import playerFactory from './playerFactory';
-import gameBoardFactory from './gameBoardFactory';
+const playerBoard = document.getElementById('playerBoard');
+const AIBoard = document.getElementById('AIBoard');
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('div');
+  row.id = 'row' + i;
+  for (let j = 0; j < 10; j++) {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = 'box' + i + j;
+    row.appendChild(box);
+  }
+  playerBoard.appendChild(row);
+}
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('div');
+  row.id = 'row' + i;
+  for (let j = 0; j < 10; j++) {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.id = 'box' + i + j;
+    row.appendChild(box);
+  }
+  AIBoard.appendChild(row);
+}
 
